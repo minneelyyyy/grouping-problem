d2e62b96a2316f2fc494126db7761ca2b1749320
minneelyyyy <aprilkahny@gmail.com>
2023-04-16 15:39:51 -0400

    update haskell to include an isPrime check

--- Haskell/calculate.hs
+++ Haskell/calculate.hs
@@ -1,9 +1,14 @@
 import System.Environment (getArgs)
-import Control.Monad (forM_)
+import Data.Foldable (for_)
+
+isPrime :: Int -> Bool
+isPrime n = null [x | x <- [2..n], n `mod` x == 0]
 
 calculate :: Int -> [Int]
 calculate n = 
-    if even n then [x | x <- [1..n `div` 2], n `mod` x == 0 && even (n `div` x)]
+    if even n then 
+        if isPrime n then [1, n `div` 2]
+        else [x | x <- [1..n `div` 2], n `mod` x == 0 && even (n `div` x)]
     else []
 
 main :: IO ()
@@ -11,6 +16,6 @@ main = do
     args <- getArgs
     let count = read $ head args :: Int
 
-    forM_ [x | x <- [1..count], even x] (\i -> do
+    for_ [x | x <- [1..count], even x] (\i -> do
             putStrLn $ show i ++ ": " ++ show (calculate i)
         )
